bb6312f4917f5e5071d488b5b3b5942a42d40031
minyifei <nova_minyf@126.com>
2019-02-14 14:51:49 +0800

    增加工厂方法模式

---
+++ src/BehavioralPatterns/FactoryMethod/Phone.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Demo\BehavioralPatterns\FactoryMethod;
+
+/**
+ * Class [Phone]
+ * 电话接口
+ * author minyifei
+ * date 2019/2/14
+ */
+
+interface Phone
+{
+
+    /**
+     * Function:call
+     * 打电话
+     *
+     * @param string $number 电话号码
+     *
+     * @return mixed
+     */
+    function call($number);
+
+}
